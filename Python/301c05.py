31df1cd019062faf4b02005c191efa8c718e2007
derrius.dawson <derrius.dawson@gmail.com>
2023-06-14 20:39:16 -0400

    301 Challenge 05

--- Python/301c05.py
+++ Python/301c05.py
@@ -1,38 +1,36 @@
 # Write a program that works out whether if a given number is an 
 # odd or even number
 # 🚨 Don't change the code below 👇
-number = int(input("Which number do you want to check? "))
+number = int(input("Which number do you want to check?"))
 # 🚨 Don't change the code above 👆
-​
-#Write your code below this line 👇
-​if (number % 2) == 0:
-    print(f"The number {number} is even.")
-else
-    print(f"The number {number} is odd.")
-​
-​
-​
-​
-​
 
-​
-​
-​
-​
+#Write your code below this line 👇
+
+if number%2==0:
+    print(f"The number {number} is even.")
+else:
+    print(f"The number {number} is odd.")
+
+
 # Create a program using maths and f-Strings that tells us how many 
 # days, weeks, months we have left if we live until 90 years old.
-​
+
 # It will take your current age as the input and output a message 
 # with our time left in this format:
-​
+
 # You have x days, y weeks, and z months left.
-​
+
 # Where x, y and z are replaced with the actual calculated numbers.
-​
-​
+
+2
 # 🚨 Don't change the code below 👇
 age = int(input("What is your current age? "))
 # 🚨 Don't change the code above 👆
-​
+
 #Write your code below this line 👇
-​
+years_left = 90 - age
+months_left = years_left * 12
+weeks_left = years_left * 52
+days_left = years_left * 365
+
+print(f"You have {days_left} days, {weeks_left} weeks, and {months_left} months left.")
